df8bccc17d328a81f49306a06d08095233c484fc
IlyaVinigradov <vivpost@mail.ru>
2023-12-06 22:40:52 +0300

    ImportError: cannot import name AddWin from partially initialized module add_win (most likely due to a circular import) + so I’ll redo the main screen + I don’t like the architecture, I should apply a pattern

--- time_management/add_win.py
+++ time_management/add_win.py
@@ -1,6 +1,17 @@
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
+import json
+import os
+from pathlib import Path
+from datetime import datetime
+from main import MainWin
+
+
+# читаем файл
+DIR = Path(__file__).resolve().parent  # путь до текущей папки
+# print(DIR)
+os.chdir(DIR)  # перейти в папку по пути
 
 
 class AddWin(QWidget):
@@ -18,18 +29,74 @@ class AddWin(QWidget):
         """ метод для формирования окна """
         # создание названия окна
         self.setWindowTitle('My time')
-        # размеры окна
-        self.resize(500, 300)
         # цвет заднего фона
         self.setStyleSheet("background-color: rgb(207, 208, 118)")
-        # координаты появления окна
-        self.move(200, 100)
+        # один метод для размеров экрана и появления в координатах
+        # setGeometry(x, y, width, height)
+        self.setGeometry(200, 129, 500, 300)
 
     def initUI(self):
-        pass
+        # надпись
+        self.label = QLabel('Введите отработанное время')
+        # поле ввода
+        self.my_time = QSpinBox()
+        # задаю минимальный диапазон
+        self.my_time.setMinimum(0)
+        # кнопка для сохранения
+        self.btn_save = QPushButton('Сохранить')
+        # кнопка назад
+        self.btn_back = QPushButton('Назад')
+
+        # лэйаут для расположения виджетов на экране
+        self.c_line = QVBoxLayout()
+        self.c_line.addWidget(
+            self.label, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.c_line.addWidget(
+            self.my_time, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.c_line.addWidget(
+            self.btn_save, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.c_line.addWidget(
+            self.btn_back, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.setLayout(self.c_line)
 
     def connects(self):
-        pass
+        """ метод нажатие на кнопку т.е. сохранение и назад"""
+        self.btn_save.clicked.connect(self.final_move)
+        self.btn_back.clicked.connect(self.back)
+
+    def save(self):
+        """ метод записи в json файл """
+        # создаем словарь
+        self.data = {}
+        # добавляем ключ и словарь в значение
+        self.data['work'] = {}
+        # обработка исключений
+        try:
+            # открытие файла в режиме чтения
+            with open('my work time.json', 'r') as file:
+                # загрузка файла в список
+                self.data = json.load(file)
+        except FileNotFoundError:
+            pass
+        self.data['work'][datetime.today()] = self.my_time.value()
+        with open(DIR / 'my work time.json', 'w') as file:
+            json.dump(self.data, file)
+
+    def done(self):
+        """ Всплывающее окошко о выполнении """
+        self.done_win = QMessageBox()
+        self.done_win.setText('Выполнено')
+        self.done_win.exec()
+
+    def back(self):
+        """ переход на предыдущий экран """
+        self.hide()
+        self.main = MainWin()
+
+    def final_move(self):
+        """ сохранение и оповещение о готовности """
+        self.save()
+        self.done()
 
 
 if __name__ == '__main__':
---
+++ time_management/new_window.py
@@ -0,0 +1,114 @@
+# import sys
+# from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QStackedWidget
+
+
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+
+#         self.stacked_widget = QStackedWidget()
+#         self.setCentralWidget(self.stacked_widget)
+
+#         self.screen1_button = QPushButton('Открыть новый экран')
+#         self.screen1_button.clicked.connect(self.open_screen2)
+
+#         screen1_layout = QVBoxLayout()
+#         screen1_layout.addWidget(self.screen1_button)
+
+#         screen1_widget = QWidget()
+#         screen1_widget.setLayout(screen1_layout)
+
+#         self.stacked_widget.addWidget(screen1_widget)
+
+#     def open_screen2(self):
+#         self.screen2_button = QPushButton('Вернуться назад')
+#         self.screen2_button.clicked.connect(self.open_screen1)
+
+#         screen2_layout = QVBoxLayout()
+#         screen2_layout.addWidget(self.screen2_button)
+
+#         screen2_widget = QWidget()
+#         screen2_widget.setLayout(screen2_layout)
+
+#         self.stacked_widget.addWidget(screen2_widget)
+#         self.stacked_widget.setCurrentWidget(screen2_widget)
+
+#     def open_screen1(self):
+#         self.stacked_widget.setCurrentIndex(0)
+
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     window = MainWindow()
+#     window.show()
+#     sys.exit(app.exec())
+
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QStackedWidget
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.stacked_widget = QStackedWidget()
+        self.setCentralWidget(self.stacked_widget)
+
+        self.screen1_button = QPushButton('Открыть экран 2')
+        self.screen1_button.clicked.connect(self.open_screen2)
+
+        screen1_button2 = QPushButton('Открыть экран 3')
+        screen1_button2.clicked.connect(self.open_screen3)
+
+        screen1_layout = QVBoxLayout()
+        screen1_layout.addWidget(self.screen1_button)
+        screen1_layout.addWidget(screen1_button2)
+
+        screen1_widget = QWidget()
+        screen1_widget.setLayout(screen1_layout)
+
+        self.stacked_widget.addWidget(screen1_widget)
+
+    def open_screen2(self):
+        self.screen2_button = QPushButton('Вернуться на экран 1')
+        self.screen2_button.clicked.connect(self.open_screen1)
+
+        screen2_button2 = QPushButton('Открыть экран 3')
+        screen2_button2.clicked.connect(self.open_screen3)
+
+        screen2_layout = QVBoxLayout()
+        screen2_layout.addWidget(self.screen2_button)
+        screen2_layout.addWidget(screen2_button2)
+
+        screen2_widget = QWidget()
+        screen2_widget.setLayout(screen2_layout)
+
+        self.stacked_widget.addWidget(screen2_widget)
+        self.stacked_widget.setCurrentWidget(screen2_widget)
+
+    def open_screen3(self):
+        self.screen3_button = QPushButton('Вернуться на экран 1')
+        self.screen3_button.clicked.connect(self.open_screen1)
+
+        screen3_button2 = QPushButton('Вернуться на экран 2')
+        screen3_button2.clicked.connect(self.open_screen2)
+
+        screen3_layout = QVBoxLayout()
+        screen3_layout.addWidget(self.screen3_button)
+        screen3_layout.addWidget(screen3_button2)
+
+        screen3_widget = QWidget()
+        screen3_widget.setLayout(screen3_layout)
+
+        self.stacked_widget.addWidget(screen3_widget)
+        self.stacked_widget.setCurrentWidget(screen3_widget)
+
+    def open_screen1(self):
+        self.stacked_widget.setCurrentIndex(0)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
